b82d8ff836659a24d279b6f867c50d1bcc71b6fa
user <a.kovrezhenkova@gmail.com>
2026-06-07 21:05:26 +0500

    добавлено логгирование

--- traffic_analyzer.py
+++ traffic_analyzer.py
@@ -1,9 +1,22 @@
 import json
 import math
 from collections import Counter
+import logging
 
 class TrafficAnalyzer:
     def __init__(self, baseline=None, current=None, sigma=3):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        file_handler = logging.FileHandler('app.log', encoding='utf-8')
+        file_handler.setLevel(logging.DEBUG)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        self.logger.handlers.clear()
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
         self._baseline = baseline
         self._current = current
         self._sigma = sigma
@@ -18,20 +31,29 @@ class TrafficAnalyzer:
         self._std = 0
         self._threshold = 0
         self._report = {}
+        self.logger.info(f'анализатор создан ({baseline}, {current}, {sigma})')
 
     def read_data(self, filepath):
+        self.logger.debug(f'чтение файла ({filepath})')
         with open(filepath, 'r', encoding='utf-8') as f:
             data = json.load(f)
+            self.logger.debug(f'файл открыт, записей {len(data)}')
             for d in data:
                 yield d
 
     def load_data(self):
+        self.logger.info('началась загрузка данных')
         self.baseline_all = list(self.read_data(self._baseline))
+        self.logger.info('все записи baseline загружены в список')
         self.current_all = list(self.read_data(self._current))
+        self.logger.info('все записи current загружены в список')
         self._baseline_unique = set(self.baseline_all)
+        self.logger.info('уникальные записи baseline загружены в множество')
         self._current_unique = set(self.current_all)
+        self.logger.info('уникальные записи current загружены в множество')
 
     def to_dict(self):
+        self.logger.debug('началась сериализация объекта в словарь')
         return{
             "settings": {
                 "baseline": self._baseline,
@@ -56,6 +78,7 @@ class TrafficAnalyzer:
 
     @classmethod
     def from_dict(cls, data):
+        logging.info('создание объекта из словаря')
         obj = cls(
             baseline=data["settings"]["baseline"],
             current=data["settings"]["current"],
@@ -71,49 +94,69 @@ class TrafficAnalyzer:
         obj._mean = data["results"]["mean"]
         obj._std = data["results"]["std"]
         obj._threshold = data["results"]["threshold"]
+        logging.debug('объект восстановлен')
         return obj
 
     def new_device(self):
+        self.logger.info('начался поиск новых устройств')
         self._new_dev = self._current_unique - self._baseline_unique
+        self.logger.info(f'найдено новых устройств {len(self._new_dev)}')
+        self.logger.debug(f'новые ip {list(self._new_dev)}')
         return self._new_dev
 
     def missing_device(self):
+        self.logger.info('начался поиск пропавших устройств')
         self._missing_dev = self._baseline_unique - self._current_unique
+        self.logger.info(f'найдено пропавших устройств {len(self._missing_dev)}')
+        self.logger.debug(f'пропавшие ip {list(self._missing_dev)}')
         return self._missing_dev
 
     def count_connections(self, ip_list):
+        self.loggint.info(f'подсчет соединений для {len(ip_list)} записей')
         return dict(Counter(ip_list))
 
     def anomalies(self):
+        self.logger.info('начался поиск аномалий')
         baseline_count = Counter(self.baseline_all)
         count_list = list(baseline_count.values())
         if len(count_list) == 0:
+            self.logger.error('нет данных')
             raise ValueError('нет данных')
         self._mean = sum(count_list) / len(count_list)
         variance = sum((x - self._mean) ** 2 for x in count_list) / len(count_list)
         self._std = math.sqrt(variance)
         self._threshold = self._mean + self._sigma * self._std
+        self.logger.debug(f'среднее {self._mean}')
+        self.logger.debug(f'стандартное отклонение {self._std}')
+        self.logger.debug(f'порог аномалии {self._threshold}')
         current_count = Counter(self.current_all)
         self._suspicious_dev = {}
         for ip in self._new_dev:
             conn = current_count[ip]
+            self.logger.debug(f'проверка ip {ip} {conn} соединений')
             if conn > self._threshold:
                 self._suspicious_dev[ip] = {"connection": conn, "threshold": self._threshold}
-
+                self.logger.info(f'найдена аномалия {ip} ({conn} соединений)')
+        self.logger.info(f'всего подозрительных устройств {len(self._suspicious_dev)}')
         return self._suspicious_dev
 
     def save_state(self, filepath):
+        self.logger.info(f'сохранение состояния в {filepath}')
         with open(filepath, 'w', encoding='utf-8') as f:
             json.dump(self.to_dict(), f, ensure_ascii=False, indent=2)
+        self.logger.info('состояние сохранено')
 
     def load_state(self, filepath):
+        self.logger.info(f'загрузка состояния из {filepath}')
         with open(filepath, 'r', encoding='utf-8') as f:
             data = json.load(f)
 
         new_obj = self.from_dict(data)
         self.__dict__.update(new_obj.__dict__)
+        self.logger.info('состояние загружено')
 
     def report(self, output_path="report.json"):
+        self.logger.info(f'генерация отчета в {output_path}')
         report_data = {
             "settings": {
                 "baseline_file": self._baseline,
@@ -136,5 +179,5 @@ class TrafficAnalyzer:
 
         with open(output_path, 'w', encoding='utf-8') as f:
             json.dump(report_data, f, indent=2, ensure_ascii=False)
-
+        self.logger.info(f'отчет загружен в {output_path}')
         return report_data
